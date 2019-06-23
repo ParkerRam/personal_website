1862ea58f17b3115ed7fb9f52e6499d4aadfa9fd
ParkerRam <parker.ram97@gmail.com>
2019-06-22 21:21:07 -0400

    nav bar done

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-@app.route("/")
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
 def home():
-    return "Hello World"
+  return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
